343c834c0e462f5dc12eab38e0ab49fc896d3edf
=wjurkowlaniec <wojtek.jurkowlaniec@gmail.com>
2024-07-24 23:33:44 +0000

    fix numpy<>torch datatype

--- neurons/cancer_ai/base/validator.py
+++ neurons/cancer_ai/base/validator.py
@@ -263,8 +263,8 @@ class BaseValidatorNeuron(BaseNeuron):
         # Calculate the average reward for each uid across non-zero values.
         # Replace any NaN values with 0.
         raw_weights = torch.nn.functional.normalize(self.scores, p=1, dim=0)
-
         bt.logging.debug("raw_weights", raw_weights)
+        raw_weights = raw_weights.numpy()
         # TODO commented  not working
         # bt.logging.debug("raw_weight_uids", self.metagraph.uids.to("cpu"))
         # Process the raw weights to final_weights via subtensor limitations.
