ffbf0b139a1457b643455ce17ea4cedc27ee5c99
Wojtek Jurkowlaniec <wojtek.jurkowlaniec@gmail.com>
2024-07-27 14:12:59 +0200

    corrected API, typings for config

--- neurons/cancer_ai/utils/api.py
+++ neurons/cancer_ai/utils/api.py
@@ -1,48 +1,57 @@
 import requests
-from functools import wraps
-import bt  # Assuming bt is your logging module or framework
+import bittensor as bt
 
-def inject_api_key(api_key_attr):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            self = args[0]
-            if not hasattr(self, api_key_attr):
-                raise AttributeError(f"{self.__class__.__name__} object has no attribute '{api_key_attr}'")
-            api_key = getattr(self, api_key_attr)
-            if 'headers' not in kwargs:
-                kwargs['headers'] = {}
-            kwargs['headers'].update({"x-api-key": api_key})
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+
+API_TIMEOUT_S = 5
+
 
 class DatasetAPI:
+    """Class for Dataset API, which
+    - fetches synthetic data for miners
+    - fetches test data for researchers
+    """
+
     def __init__(self, base_path, api_key):
         self.base_path = base_path
         self.headers = {"x-api-key": api_key}
 
-    def get_image_data(self, amount, headers):
+    def get_image_data(self, amount):
         """Gets images from API"""
         endpoint = f"{self.base_path}/dataset/skin/melanoma?amount={amount}"
-        response = requests.get(url=endpoint, headers=self.headers)
+        response = requests.get(
+            url=endpoint, headers=self.headers, timeout=API_TIMEOUT_S
+        )
         data = response.json()
         bt.logging.debug(f"Dataset API response: {data}")
         return data
 
+
 class StatsAPI:
+    """Class for Stats API, which does the following
+    - Send testing data from researcher
+    - Fetch top researcher for incentive
+    """
+
     def __init__(self, base_path, api_key):
         self.base_path = base_path
         self.headers = {"x-api-key": api_key}
 
-    def send_researcher_scores(self, researcher_uid, researcher_scores, headers):
-
+    def send_researcher_scores(self, researcher_uid, researcher_scores):
+        """Send testing data from researcher"""
         endpoint = f"{self.base_path}/researcher/testing/{researcher_uid}"
-        res = requests.post(url=endpoint, json=researcher_scores, headers=self.headers)
+        res = requests.post(
+            url=endpoint,
+            json=researcher_scores,
+            headers=self.headers,
+            timeout=API_TIMEOUT_S,
+        )
         return res
 
-    def fetch_top_researchers(self, headers):
+    def fetch_top_researchers(self):
+        """Fetches top researcher for incentive"""
         endpoint = f"{self.base_path}/emission-share"
-        response = requests.get(url=endpoint, headers=self.headers)
+        response = requests.get(
+            url=endpoint, headers=self.headers, timeout=API_TIMEOUT_S
+        )
         data = response.json()
         return data
