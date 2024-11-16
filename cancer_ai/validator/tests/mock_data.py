425d53cb0a25e1dac787326dbb8db5faa5ea6fb9
konrad0960 <konradsebastianmolinski@gmail.com>
2024-11-16 20:32:40 +0100

    mock data updated

--- cancer_ai/validator/tests/mock_data.py
+++ cancer_ai/validator/tests/mock_data.py
@@ -2,13 +2,11 @@ from cancer_ai.validator.model_manager import ModelInfo
  
 def get_mock_hotkeys_with_models():
     return {
-        #good model
         "5HeH6kmR6FyfC6K39aGozMJ3wUTdgxrQAQsy4BBbskxHKqgG": ModelInfo(
             hf_repo_id="eatcats/test",
             hf_model_filename="melanoma-1-piwo.onnx",
             hf_repo_type="model",
         ),
-        #wrong model
         "5CQFdhmRyQtiTwHLumywhWtQYTQkF4SpGtdT8aoh3WK3E4E2": ModelInfo(
             hf_repo_id="eatcats/melanoma-test",
             hf_model_filename="2024-08-24_04-37-34-melanoma-1.onnx",
