3b9dce53550b6ad6c3fdddf520715d02f500c06f
Wojtek Jurkowlaniec <wojtek.jurkowlaniec@gmail.com>
2025-08-07 17:39:13 +0200

    miner evalutaion fix

--- neurons/miner.py
+++ neurons/miner.py
@@ -122,10 +122,10 @@ class MinerManagerCLI:
             )
             await dataset_manager.prepare_dataset()
 
-            X_test, y_test = await dataset_manager.get_data()
+            X_test, y_test, metadata = await dataset_manager.get_data()
 
             competition_handler = COMPETITION_HANDLER_MAPPING[self.config.competition_id](
-                X_test=X_test, y_test=y_test, config=self.config
+                X_test=X_test, y_test=y_test, metadata=metadata, config=self.config
             )
 
             # Set preprocessing directory and preprocess data once
